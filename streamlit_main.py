cfb9ad2f535486a3b1ac4c6e8a836b23aead36db
calkitos <168606802+calkitos@users.noreply.github.com>
2024-05-03 21:07:12 -0600

    streamlit_main.py

--- streamlit_main.py
+++ streamlit_main.py
@@ -1,7 +1,7 @@
 #-----------------------------------------------------------------
 #BIBLIOTECAS------------------------------------------------------
 #-----------------------------------------------------------------
-#!pip install streamlit
+!pip install streamlit
 import streamlit as st
 import pandas as pd
 import numpy as np
