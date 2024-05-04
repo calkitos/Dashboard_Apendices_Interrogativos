4572d44917da3988dff1d9f89f20261f679e7fa5
calkitos <168606802+calkitos@users.noreply.github.com>
2024-05-03 21:23:51 -0600

    streamlit_main.py

--- streamlit_main.py
+++ streamlit_main.py
@@ -1,13 +1,13 @@
 #-----------------------------------------------------------------
 #BIBLIOTECAS------------------------------------------------------
 #-----------------------------------------------------------------
-pip install streamlit
-import streamlit as st
-import pandas as pd
-import numpy as np
-import plotly.express as px
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
+#pip install streamlit
+#import streamlit as st
+#import pandas as pd
+#import numpy as np
+#import plotly.express as px
+#import plotly.graph_objects as go
+#from plotly.subplots import make_subplots
 
 #-----------------------------------------------------------------
 #DATOS------------------------------------------------------------
